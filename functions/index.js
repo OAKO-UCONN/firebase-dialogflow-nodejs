ba28de661f037a0be4493943f8e6ccc34295be85
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-01-06 21:19:11 -0500

    index.js

--- functions/index.js
+++ functions/index.js
@@ -16,54 +16,14 @@ exports.dialogflowGateway = functions.https.onRequest((request, response) => {
     const { queryInput, sessionId } = request.body;
 
 
-    const sessionClient = new SessionsClient({ credentials: serviceAccount });
-    const session = sessionClient.sessionPath('fireship-lessons', sessionId);
+    const sessionClient = new SessionsClient({ credentials: serviceAccount  });
+    const session = sessionClient.sessionPath('your-project', sessionId);
 
 
     const responses = await sessionClient.detectIntent({ session, queryInput});
 
     const result = responses[0].queryResult;
 
-    result.fulfillmentText
-
     response.send(result);
-
   });
-});
-
-
-const { WebhookClient } = require('dialogflow-fulfillment');
-
-exports.dialogflowWebhook = functions.https.onRequest(async (request, response) => {
-    const agent = new WebhookClient({ request, response });
-
-    console.log(JSON.stringify(request.body));
-
-    const result = request.body.queryResult;
-   
-    function welcome(agent) {
-      agent.add(`Welcome to my agent!`);
-    }
-   
-    function fallback(agent) {
-      agent.add(`Sorry, can you try again?`);
-    }
-
-    async function userOnboardingHandler(agent) {
-
-     const db = admin.firestore();
-     const profile = db.collection('users').doc('jeffd23');
-
-     const { name, color } = result.parameters;
-
-      await profile.set({ name, color })
-      agent.add(`Welcome aboard my friend!`);
-    }
-
-
-    let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
-    intentMap.set('UserOnboarding', userOnboardingHandler);
-    agent.handleRequest(intentMap);
 });
